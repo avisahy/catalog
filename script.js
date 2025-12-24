1ccac9f0031e49e759fc63e2da34ddab873c3bb2
avisahy <avisahy001@gmail.com>
2025-12-24 09:30:10 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelectorAll(".card").forEach(card => {
+  card.addEventListener("mousemove", e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left - rect.width / 2;
+    const y = e.clientY - rect.top - rect.height / 2;
+
+    const rotateX = (y / rect.height) * -20;
+    const rotateY = (x / rect.width) * 20;
+
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "rotateX(0deg) rotateY(0deg)";
+  });
+});
